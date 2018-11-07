c2e3c5f1b734adcf6eea0991a2841f6873e44a0f
kennedyeduardo1996 <kennedyeduardomartins@gmail.com>
2018-11-07 00:42:50 -0200

    botao mudar

--- owlcarousel2/caro2.js
+++ owlcarousel2/caro2.js
@@ -29,13 +29,64 @@
             items:1,
             loop:true,
         });
-        $("#owl-seta").owlCarousel({
-            center: true,
-            items:2,
+        var owlSeta = $("#owl-seta").owlCarousel({
+            stagePadding: 135,
             loop:true,
+            nav:false,
+            margin:0,
+            items:1,
+            lazyLoad: true,
+            nav:true
         });
+
+        $('button.next').click(function() {
+            owlSeta.trigger('next.owl.carousel');
+        })
+
+        $('button.prev').click(function() {
+            owlSeta.trigger('prev.owl.carousel', [600]);
+        })
         // fim carousel
 
+    });
+    $('.caro3d').owlCarousel({
+        stagePadding: 200,
+        loop:true,
+        margin:10,
+        nav:false,
+        items:1,
+        lazyLoad: true,
+        nav:true
+        // responsive:{
+        //     0:{
+        //         items:1,
+        //         stagePadding: 60
+        //     },
+        //     600:{
+        //         items:1,
+        //         stagePadding: 100
+        //     },
+        //     1000:{
+        //         items:1,
+        //         stagePadding: 200
+        //     },
+        //     1200:{
+        //         items:1,
+        //         stagePadding: 250
+        //     },
+        //     1400:{
+        //         items:1,
+        //         stagePadding: 300
+        //     },
+        //     1600:{
+        //         items:1,
+        //         stagePadding: 350
+        //     },
+        //     1800:{
+        //         items:1,
+        //         stagePadding: 400
+        //     }
+        // }
     });
 
 
@@ -45,8 +96,17 @@
 
 $(window).resize(function(){
     menuNav();
+    bannerContainer();
 
 });
+
+function bannerContainer() {
+var containerMax = $('.container').width();
+    $('.active .item').css({'width':containerMax});
+
+};
+
+
 function menuNav() {
     var widthMax = $(window).width();
     if (widthMax <= 768){
